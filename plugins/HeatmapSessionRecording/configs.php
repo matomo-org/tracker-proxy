17956f4387df1c4b1adeca16433d2d6668b1233a
diosmosis <diosmosis@users.noreply.github.com>
2018-04-16 16:53:20 -0700

    Move bulk of proxying logic to proxy.php and use that in piwik.php. This way `$path` is always defined.

--- plugins/HeatmapSessionRecording/configs.php
+++ plugins/HeatmapSessionRecording/configs.php
@@ -2,8 +2,6 @@
 
 # proxy endpoint to support HeatmapSessionRecording tracker which sends requests to this file
 
-define('DISABLE_SEND_IMAGE', 1);
-
 $path = 'plugins/HeatmapSessionRecording/configs.php';
 
-include dirname(__FILE__) . '/../../piwik.php';
+include dirname(__FILE__) . '/../../proxy.php';
