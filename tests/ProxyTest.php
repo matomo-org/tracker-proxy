45830090c0d6b2b186fb9119596dbcacf345835e
Nathan Gavin <nathangavin987@gmail.com>
2025-02-27 10:35:58 +1300

    made minor changes, moved phpcs to separate action

--- tests/ProxyTest.php
+++ tests/ProxyTest.php
@@ -355,11 +355,6 @@ RESPONSE;
             $matomoUrl = $this->getMatomoUrl();
         }
 
-        if ($forceIpV6) {
-            // on travis, using an ipv6 ip address causes an error when resolving localhost
-            //$matomoUrl = str_replace("localhost", "127.0.0.1", $matomoUrl);
-        }
-
         $client = new Client();
 
         if (!$path) {
